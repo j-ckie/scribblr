7ff6b5124bf3394a9d372b56cd7253e49c88c236
Jaclyn Legacion <jglegacion@gmail.com>
2020-02-20 09:50:00 -0600

    add console.log to test edit details

--- client/src/components/EditDetails.js
+++ client/src/components/EditDetails.js
@@ -72,6 +72,7 @@ export class EditDetails extends Component {
     }
 
     render() {
+        console.log("testing edit profile");
         const { classes } = this.props;
         return (
             <Fragment>
