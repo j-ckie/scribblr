f76f3cfb3a402ba1008919e6e0946b2db0ff8ab1
Jaclyn Legacion <jglegacion@gmail.com>
2020-02-21 10:03:24 -0600

    add access control header to server

--- server/functions/index.js
+++ server/functions/index.js
@@ -5,6 +5,12 @@
 const express = require("express"); // requires and calls express in one line
 const app = express();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // firebase functions setup
 const functions = require("firebase-functions");
 
